5bf2e0219f3d7d2ce7fef62a41c94a9645794c89
heping9807 <1399973955@qq.com>
2019-12-10 09:00:48 +0800

    新建按钮成功，可以打开日历，listview初始化成功，

--- time1/app/src/main/java/com/example/time1/NewMainActivity.java
+++ time1/app/src/main/java/com/example/time1/NewMainActivity.java
@@ -9,11 +9,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,13 +42,23 @@ public class NewMainActivity extends AppCompatActivity {
             }
         });
 
-        init();
+        initTime();
+
+        TimeAdapter adapter=new TimeAdapter(NewMainActivity.this ,R.layout.list_view_item_time ,listTimes);
         ListView listViewTime=this.findViewById(R.id.listview);
-        TimeAdapter adapter=new TimeAdapter(NewMainActivity.this ,android.R.layout.activity_list_item ,listTimes);
         listViewTime.setAdapter(adapter);
+        /////////listview的点击响应函数，由position判断点击的是哪个子项
+        listViewTime.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Time list_time = listTimes.get(position);
+                Toast.makeText(NewMainActivity.this, list_time.getTitle(),
+                        Toast.LENGTH_SHORT).show();
+            }
+            });
     }
 
-    private void init() {
+    private void initTime() {
         listTimes.add(new Time("1月纪念日","100 days",R.drawable.time_1));
         listTimes.add(new Time("2月纪念日","100 days",R.drawable.time_2));
     }
@@ -55,19 +69,27 @@ public class NewMainActivity extends AppCompatActivity {
     class TimeAdapter extends ArrayAdapter<Time> {
         private int resourceId;
 
-        public TimeAdapter(Context context, int resource, List<Time> objects) {
+        TimeAdapter(Context context, int resource, List<Time> objects) {
             super(context, resource, objects);
             resourceId = resource;
         }
+
         @NonNull
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             Time time = getItem(position);//获取当前项的实例
-            View view = LayoutInflater.from(this.getContext()).inflate(this.resourceId, null);
+            LayoutInflater mInflater=LayoutInflater.from(this.getContext());
+            View view = mInflater.inflate(this.resourceId, null);
 
-            ImageView imageView=(ImageView)view.findViewById(R.id.image_view_time_cover);
-            TextView textView_title=(TextView)view.findViewById(R.id.text_view_time_title);
-            TextView textView_shijian=(TextView)view.findViewById(R.id.text_view_time_shijian);
+            ImageView imageView=(ImageView)view.findViewById(R.id.image_view_time_cover);//图片
+            imageView.setImageResource(time.getCoverResourceId());
+            TextView textView_title=(TextView)view.findViewById(R.id.text_view_time_title);//标题
+            textView_title.setText(time.getTitle());
+            TextView textView_shijian=(TextView)view.findViewById(R.id.text_view_time_shijian);//时间
+            textView_shijian.setText(time.getTime());
+
+
+
             return view;
         }
     }
