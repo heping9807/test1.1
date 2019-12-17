2b0910481db1d5f83c67e9340f7dc3e6f7b9dd5a
heping9807 <1399973955@qq.com>
2019-12-17 16:57:01 +0800

    新建功能未实现,详情界面布局完成，接下来做intent传参,部分传值成功，准备做倒计时列表

--- time1/app/src/main/java/com/example/time1/ui/shike/HomeFragment.java
+++ time1/app/src/main/java/com/example/time1/ui/shike/HomeFragment.java
@@ -16,6 +16,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,12 +32,47 @@ import com.example.time1.Time;
 import com.example.time1.TimeMainActivity;
 import com.example.time1.ui.shezhi.SendViewModel;
 
+import static android.app.Activity.RESULT_OK;
+
 
 public class HomeFragment extends Fragment {
+    public static final int REQUEST_CODE_GET_ALL = 903;
+    public static final int REQUEST_CODE_GET_COLOR = 904;
     private SendViewModel sendViewModel;
     public Button New_button;
     public List<Time> listTimes=new ArrayList<>();
-    TimeAdapter adapter;
+    TimeAdapter timeadapter;
+    int nian,yue,ri;        //年月日
+    private Timer timer = null;
+    private TimerTask task = null;
+
+    @Override       //传值
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_CODE_GET_ALL:
+                if(resultCode==RESULT_OK){
+                    //取得启动该Activity的Intent对象
+
+                    nian = data.getIntExtra("nian",0);
+                    yue = data.getIntExtra("yue",0);
+                    ri = data.getIntExtra("ri",0);
+                    String biaoti=data.getStringExtra("biaoti");
+                    String beizhu=data.getStringExtra("beizhu");
+
+                    listTimes.add(new Time("biaoti","100 days",R.drawable.time_1));
+                    timeadapter.notifyDataSetChanged();
+                    Toast.makeText(this, "新建ListView成功", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case REQUEST_CODE_GET_COLOR:
+                if(resultCode==RESULT_OK){
+
+
+                }
+                break;
+        }
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -42,22 +80,22 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(SendViewModel.class);
         View root = inflater.inflate(R.layout.fragment_2_shike,container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-
         New_button = root.findViewById(R.id.New_button);
-        New_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(HomeFragment.this.getActivity(), TimeMainActivity.class);
-                startActivity(intent);
-            }
-        });
-
         initTime();
 
-        adapter = new TimeAdapter(HomeFragment.this.getActivity(), R.layout.list_view_item_time, listTimes);
+        timeadapter = new TimeAdapter(HomeFragment.this.getActivity(), R.layout.list_view_item_time, listTimes);
         ListView listViewTime = root.findViewById(R.id.listview);
-        listViewTime.setAdapter(adapter);
+        listViewTime.setAdapter(timeadapter);
+        /*
+        //取得启动该Fragment的对象
+        Bundle arguments = getArguments();
+        String biaoti = arguments.getString("biaoti");
+        String beizhu = arguments.getString("beizhu");
+        listTimes.add(new Time(biaoti,beizhu,R.drawable.time_1));
+        timeadapter.notifyDataSetChanged();
+        */
+
+
 
         /////////listview的点击响应函数，由position判断点击的是哪个子项
         listViewTime.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -69,6 +107,15 @@ public class HomeFragment extends Fragment {
                 Intent intent1 = new Intent();
                 intent1.setClass(HomeFragment.this.getActivity(), DetailMainActivity.class);
                 startActivity(intent1);
+            }
+        });
+
+        New_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(HomeFragment.this.getActivity(), TimeMainActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_GET_ALL);
             }
         });
         return root;
@@ -109,70 +156,4 @@ public class HomeFragment extends Fragment {
     }
 
 }
-/*
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_2_shike);
 
-        New_button = this.findViewById(R.id.New_button);
-        New_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(HomeFragment.this, TimeMainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        initTime();
-
-        TimeAdapter adapter=new TimeAdapter(HomeFragment.this ,R.layout.list_view_item_time ,listTimes);
-        ListView listViewTime=this.findViewById(R.id.listview);
-        listViewTime.setAdapter(adapter);
-        /////////listview的点击响应函数，由position判断点击的是哪个子项
-        listViewTime.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Time list_time = listTimes.get(position);
-                Toast.makeText(HomeFragment.this, list_time.getTitle(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void initTime() {
-        listTimes.add(new Time("1月纪念日","100 days",R.drawable.time_1));
-        listTimes.add(new Time("2月纪念日","100 days",R.drawable.time_2));
-    }
-
-    public List<Time> getListTimes(){
-        return listTimes;
-    }
-    class TimeAdapter extends ArrayAdapter<Time> {
-        private int resourceId;
-
-        TimeAdapter(Context context, int resource, List<Time> objects) {
-            super(context, resource, objects);
-            resourceId = resource;
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            Time time = getItem(position);//获取当前项的实例
-            LayoutInflater mInflater=LayoutInflater.from(this.getContext());
-            View view = mInflater.inflate(this.resourceId, null);
-
-            ImageView imageView=(ImageView)view.findViewById(R.id.image_view_time_cover);//图片
-            imageView.setImageResource(time.getCoverResourceId());
-            TextView textView_title=(TextView)view.findViewById(R.id.text_view_time_title);//标题
-            textView_title.setText(time.getTitle());
-            TextView textView_shijian=(TextView)view.findViewById(R.id.text_view_time_shijian);//时间
-            textView_shijian.setText(time.getTime());
-
-
-
-            return view;
-        }
-    }*/
