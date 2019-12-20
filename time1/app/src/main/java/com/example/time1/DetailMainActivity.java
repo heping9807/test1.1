60d4cb94e11431880fc21f02017dd87fdd50d6b0
heping9807 <1399973955@qq.com>
2019-12-20 12:46:17 +0800

    基本功能都已实现,可以选择时间到时/分

--- time1/app/src/main/java/com/example/time1/DetailMainActivity.java
+++ time1/app/src/main/java/com/example/time1/DetailMainActivity.java
@@ -5,18 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DetailMainActivity extends AppCompatActivity {
     public static final int RESULT_CODE_DELETE = 907;
     public EditText text_biaoti,text_shijian,text_beizhu;
+    public TextView textView_show;
     public Button button_delete,button_change;
     private int editPosition;
+    CountDownTimer Timer;
+    String Nowtime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,16 +37,23 @@ public class DetailMainActivity extends AppCompatActivity {
         text_biaoti=findViewById(R.id.editText_biaoti);
         text_shijian=findViewById(R.id.editText_time);
         text_beizhu=findViewById(R.id.editText_beizhu);
+        textView_show=findViewById(R.id.textView_show);
 
         editPosition=getIntent().getIntExtra("edit_position",0);
         String goodName= getIntent().getStringExtra("title");
         String goodTime= getIntent().getStringExtra("time");
         String goodBeizhu= getIntent().getStringExtra("beizhu");
+        Nowtime= getIntent().getStringExtra("Nowtime");
 
         if(goodName!=null) {
             text_biaoti.setText(goodName);
             text_shijian.setText(goodTime+"");
             text_beizhu.setText(goodBeizhu);
+        }
+        try {
+            initData();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
         final ListView listViewTime = findViewById(R.id.listview);
@@ -78,5 +95,34 @@ public class DetailMainActivity extends AppCompatActivity {
                         .create().show();
             }
         });
+    }
+    public void initData() throws ParseException {
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+        Date date =sdf.parse(Nowtime);
+        Calendar calendar = Calendar.getInstance();
+        assert date != null;
+        calendar.setTime(date);
+        long timeSave=calendar.getTimeInMillis()-System.currentTimeMillis();
+
+        if(Timer!=null)
+            Timer.cancel();
+        Timer = new CountDownTimer(timeSave, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                    long day = millisUntilFinished / (1000 * 24 * 60 * 60); //单位天
+
+                    long hour = (millisUntilFinished - day * (1000 * 24 * 60 * 60)) / (1000 * 60 * 60);
+                    //单位时
+                    long minute = (millisUntilFinished - day * (1000 * 24 * 60 * 60) - hour * (1000 * 60 * 60)) / (1000 * 60);
+                    //单位分
+                    long second = (millisUntilFinished - day * (1000 * 24 * 60 * 60) - hour * (1000 * 60 * 60) - minute * (1000 * 60)) / 1000;
+                    //单位秒
+                    textView_show.setText(day+"天"+hour + "小时" + minute + "分钟" + second + "秒");
+            }
+            @Override
+            public void onFinish() {
+            }
+        };
+        Timer.start();
     }
 }
